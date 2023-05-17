87cb9929714d8bb378c8709dd0b436c49de976b0
Chamal Dissanayake <65418556+chamal-dissanayake@users.noreply.github.com>
2023-05-18 00:38:59 +0530

    update
    change route

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -44,7 +44,7 @@ const Blog = () => {
       />
     ))}
     
-    <img src='https://media.springernature.com/full/springer-static/image/art%3A10.1186%2Fs12882-019-1501-0/MediaObjects/12882_2019_1501_Fig2_HTML.png?as=webp' className="image" alt='blog'></img>
+    <img src='https://media.springernature.com/full/springer-static/image/art%3A10.1186%2Fs12882-019-1501-0/MediaObjects/12882_2019_1501_Fig2_HTML.png?as=webp' className="image" alt='blog_images'></img>
     </Col>
   </Row>  
   </div>
