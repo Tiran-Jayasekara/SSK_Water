c1b7279afc882506f10716ed2c2c1cda3ed58857
Tiran Jayasekara <70306643+Tiran2385@users.noreply.github.com>
2023-06-10 00:26:55 +0530

    update store
    update store

--- src/pages/Store/ProductList.js
+++ src/pages/Store/ProductList.js
@@ -58,20 +58,20 @@ function ProductList(props) {
     let productList;
 
     switch (props.Category) {
-    case "Osmosis":
-     productList = Osmosis;
+    case "Domestic":
+     productList = Domestic;
      break;
-    case "Ionizers":
-        productList = Ionizers;
+    case "Commercial":
+        productList = Commercial;
         break;
-    case "Distillation":
-        productList = Distillation;
+    case "Ionizer":
+        productList = Ionizer;
         break;
-    case "Ceramic":
-        productList = Ceramic;
+    case "Spare":
+        productList = Spare;
         break;
     default:
-        productList = [];
+        productList = Industrial;
     }
   
     return (
@@ -87,7 +87,7 @@ function ProductList(props) {
               <img className="product-image" src={product.image} alt={product.title} onClick={() => imageClick(product)} />
               <h5 className="product-title">{product.title}</h5>
               <div className="product-details">
-                <p className="product-price">${product.price}</p>
+                <p className="product-price">RS : {product.price}</p>
               </div>
             </div>
           </Col>
@@ -228,212 +228,244 @@ function ProductList(props) {
 
 
 
-  const Osmosis = [
+  const Domestic = [
     {
       id: 1,
-      title: "Osmosis 1 ",
+      title: "Domestic 1 ",
       author: "Douglas Adams",
-      price: 10.99,
+      price: 85000,
       image: "https://m.media-amazon.com/images/I/71Iq1Ihu4fL._SL1500_.jpg",
       describe:"A water filter is a device that removes impurities and contaminants from water by means of a physical barrier, a chemical process, or a biological process. The purpose of a water filter is to improve the quality of the water by removing unwanted substances such as sediment, chlorine, bacteria, viruses, heavy metals, and other harmful chemicals."
     },
     {
       id: 2,
-      title: "Osmosis 2 ",
+      title: "Domestic 2 ",
       author: "Harper Lee",
-      price: 7.99,
+      price: 85000,
       image: "https://cdn11.bigcommerce.com/s-mpfo2gcqca/images/stencil/1280x1280/products/476/2363/brondell-capella-RC250-reverse-osmosis-water-filtration-system-white__88084.1660687393.jpg?c=1"
     },
     {
       id: 3,
-      title: "Osmosis 3 ",
+      title: "Domestic 3 ",
       author: "F. Scott ",
-      price: 12.99,
+      price: 85000,
       image: "https://naturerowater.com/wp-content/uploads/2020/01/0-kent-Excell-ro-water-filter-900x900-1.jpg"
     },
     {
       id: 4,
-      title: "Osmosis 4 ",
+      title: "Domestic 4 ",
       author: "F. Scott ",
-      price: 12.99,
+      price: 85000,
       image: "https://www.wawaterfilters.com.au/wp-content/uploads/2011/12/Autoclave-Zero-TDS-Reverse-Osmosis-180722-1428-max-min.jpg"
     },{
       id: 5,
-      title: "Osmosis 1 ",
+      title: "Domestic 1 ",
       author: "Douglas Adams",
-      price: 10.99,
+      price: 85000,
       image: "https://m.media-amazon.com/images/I/71Iq1Ihu4fL._SL1500_.jpg"
     },
     {
       id: 6,
-      title: "Osmosis 2 ",
+      title: "Domestic 2 ",
       author: "Harper Lee",
-      price: 7.99,
+      price: 85000,
       image: "https://cdn11.bigcommerce.com/s-mpfo2gcqca/images/stencil/1280x1280/products/476/2363/brondell-capella-RC250-reverse-osmosis-water-filtration-system-white__88084.1660687393.jpg?c=1"
     },
     {
       id: 7,
-      title: "Osmosis 3 ",
+      title: "Domestic 3 ",
       author: "F. Scott ",
-      price: 12.99,
+      price: 85000,
       image: "https://naturerowater.com/wp-content/uploads/2020/01/0-kent-Excell-ro-water-filter-900x900-1.jpg"
     },
     {
       id: 8,
-      title: "Osmosis 4 ",
+      title: "Domestic 4 ",
       author: "F. Scott ",
-      price: 12.99,
+      price: 85000,
       image: "https://www.wawaterfilters.com.au/wp-content/uploads/2011/12/Autoclave-Zero-TDS-Reverse-Osmosis-180722-1428-max-min.jpg"
     },
   ];
 
-  const Ionizers = [
+  const Commercial = [
       {
         id: 1,
-        title: "Ionizers 1",
+        title: "Commercial 1",
         author: "Douglas Adams",
         price: 10.99,
         image:"https://waterionizer.org/wp-content/uploads/best-water-filters1.jpg"
       },
       {
         id: 2,
-        title: "Ionizers 2",
+        title: "Commercial 2",
         author: "Harper Lee",
         price: 7.99,
         image:"https://m.media-amazon.com/images/I/61XP6k4LhJL._AC_UF1000,1000_QL80_.jpg"
       },
       {
         id: 3,
-        title: "Ionizers 3",
+        title: "Commercial 3",
         author: "F. Scott Fitzgerald",
         price: 12.99,
         image:"https://m.media-amazon.com/images/I/81dXerYATVL.jpg"
       },
       {
         id: 4,
-        title: "Ionizers 4",
+        title: "Commercial 4",
         author: "F. Scott Fitzgerald",
         price: 12.99,
         image:"https://i5.walmartimages.com/asr/b20d86f4-a146-4e9c-a5ce-ae1743ab468b.5562a4a4c06e9fed73187456ae5ab223.jpeg?odnHeight=580&odnWidth=580&odnBg=FFFFFF"
       },{
         id: 5,
-        title: "Ionizers 1",
+        title: "Commercial 1",
         author: "Douglas Adams",
         price: 10.99,
         image:"https://waterionizer.org/wp-content/uploads/best-water-filters1.jpg"
       },
       {
         id: 6,
-        title: "Ionizers 2",
+        title: "Commercial 2",
         author: "Harper Lee",
         price: 7.99,
         image:"https://m.media-amazon.com/images/I/61XP6k4LhJL._AC_UF1000,1000_QL80_.jpg"
       },
       {
         id: 7,
-        title: "Ionizers 3",
+        title: "Commercial 3",
         author: "F. Scott Fitzgerald",
         price: 12.99,
         image:"https://m.media-amazon.com/images/I/81dXerYATVL.jpg"
       },
       {
         id: 8,
-        title: "Ionizers 4",
+        title: "Commercial 4",
         author: "F. Scott Fitzgerald",
         price: 12.99,
         image:"https://i5.walmartimages.com/asr/b20d86f4-a146-4e9c-a5ce-ae1743ab468b.5562a4a4c06e9fed73187456ae5ab223.jpeg?odnHeight=580&odnWidth=580&odnBg=FFFFFF"
       },
     ];
 
-    const Distillation = [
+    const Ionizer = [
       {
         id: 1,
-        title: "Distillation 1",
+        title: "Ionizer 1",
         author: "Douglas Adams",
         price: 10.99,
         image:"https://sc04.alicdn.com/kf/H2122d137337946e9a5bd403ddc9a97cdv/251009102/H2122d137337946e9a5bd403ddc9a97cdv.jpg"
       },
       {
         id: 2,
-        title: "Distillation 2",
+        title: "Ionizer 2",
         author: "Harper Lee",
         price: 7.99,
         image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTziXyCe6M7AuhpZ4CEUk3n9RDXBIGRSDnLi2hiuyKONddba1QaOPbsddDLHHSxLVqoW8s&usqp=CAU"
       },
       {
         id: 3,
-        title: "Distillation 3",
+        title: "Ionizer 3",
         author: "F. Scott Fitzgerald",
         price: 12.99,
         image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2shXuR1ZhPZgqjVTxI3F9JFoFZyOyhCGh9mpviIVSOcgOz7NGHk_x-ZpXtcfY3XJo1zE&usqp=CAU"
       },
       {
         id: 4,
-        title: "Distillation 4",
+        title: "Ionizer 4",
         author: "F. Scott Fitzgerald",
         price: 12.99,
         image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9TM-Sy373RU5_BRlBm33btVfiuqGDg6Ze35Cj1_bSo9WFvH9-A1xzsJdNKuZ2xEmT9-k&usqp=CAU"
       },
       {
         id: 5,
-        title: "Distillation 1",
+        title: "Ionizer 1",
         author: "Douglas Adams",
         price: 10.99,
         image:"https://sc04.alicdn.com/kf/H2122d137337946e9a5bd403ddc9a97cdv/251009102/H2122d137337946e9a5bd403ddc9a97cdv.jpg"
       },
       {
         id: 6,
-        title: "Distillation 2",
+        title: "Ionizer 2",
         author: "Harper Lee",
         price: 7.99,
         image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTziXyCe6M7AuhpZ4CEUk3n9RDXBIGRSDnLi2hiuyKONddba1QaOPbsddDLHHSxLVqoW8s&usqp=CAU"
       },
       {
         id: 7,
-        title: "Distillation 3",
+        title: "Ionizer 3",
         author: "F. Scott Fitzgerald",
         price: 12.99,
         image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2shXuR1ZhPZgqjVTxI3F9JFoFZyOyhCGh9mpviIVSOcgOz7NGHk_x-ZpXtcfY3XJo1zE&usqp=CAU"
       },
       {
         id: 8,
-        title: "Distillation 4",
+        title: "Ionizer 4",
         author: "F. Scott Fitzgerald",
         price: 12.99,
         image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9TM-Sy373RU5_BRlBm33btVfiuqGDg6Ze35Cj1_bSo9WFvH9-A1xzsJdNKuZ2xEmT9-k&usqp=CAU"
       },
     ];
 
-    const Ceramic = [
+    const Spare = [
       {
         id: 13,
-        title: "Ceramic 1",
+        title: "Spare 1",
         author: "Douglas Adams",
         price: 10.99,
         image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhy-3G8I6RCqLIQOqv1NzSUbe4O2NxyPTZwQ&usqp=CAU"
       },
       {
         id: 14,
-        title: "Ceramic 2",
+        title: "Spare 2",
         author: "Harper Lee",
         price: 7.99,
         image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPcXCHl7VsM9ogGXSy9cFQKHgTCFY7bwqnVUB3fBiHchl1OkRMS8DDi9fu12lfbNXrWPM&usqp=CAU"
       },
       {
         id: 15,
-        title: "Ceramic 3",
+        title: "Spare 3",
         author: "F. Scott Fitzgerald",
         price: 12.99,
         image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkA25k_qXXxSjY70j77YHjjCjKiZ88rcMtjrfg1gdjeHfnr06nTnJcq7QE-gyhqlCBUUU&usqp=CAU"
       },
       {
         id: 16,
-        title: "Charger 4",
+        title: "Spare 4",
         author: "F. Scott Fitzgerald",
         price: 12.99,
         image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRcenTO2gmA-QsPMO6xJn4mznzVzfRsndQak9eGegYY_yp1sC15dxdB13kqBLPefhyz7g&usqp=CAU"
       },
     ];
+
+    const Industrial = [
+      {
+        id: 13,
+        title: "Industrial 1",
+        author: "Douglas Adams",
+        price: 10.99,
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhy-3G8I6RCqLIQOqv1NzSUbe4O2NxyPTZwQ&usqp=CAU"
+      },
+      {
+        id: 14,
+        title: "Industrial 2",
+        author: "Harper Lee",
+        price: 7.99,
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPcXCHl7VsM9ogGXSy9cFQKHgTCFY7bwqnVUB3fBiHchl1OkRMS8DDi9fu12lfbNXrWPM&usqp=CAU"
+      },
+      {
+        id: 15,
+        title: "Industrial 3",
+        author: "F. Scott Fitzgerald",
+        price: 12.99,
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkA25k_qXXxSjY70j77YHjjCjKiZ88rcMtjrfg1gdjeHfnr06nTnJcq7QE-gyhqlCBUUU&usqp=CAU"
+      },
+      {
+        id: 16,
+        title: "Industrial 4",
+        author: "F. Scott Fitzgerald",
+        price: 12.99,
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRcenTO2gmA-QsPMO6xJn4mznzVzfRsndQak9eGegYY_yp1sC15dxdB13kqBLPefhyz7g&usqp=CAU"
+      },
+    ];
+
 
   
   export default ProductList;
--- src/pages/Store/Store.js
+++ src/pages/Store/Store.js
@@ -21,25 +21,25 @@ useEffect(() => {
   return (
     <>
     <section>
-    <Container className="Container-custom">
+  <Container className="Container-custom">
   <Row className="product-buttons-row">
     <Col lg="3" sm ="12" xs="12" className="my-col">
       <div className="Product_NavBar">
         <ul className="d-flex">
           <li>
-            <Button onClick={() => { SetData(<ProductList Category={'Osmosis'} />) }}>Osmosis</Button>
+            <Button onClick={() => { SetData(<ProductList Category={'Domestic'} />) }} >Domestic RO</Button>
           </li>
           <li>
-            <Button onClick={() => { SetData(<ProductList Category={'Ionizers'} />) }}>Ionizers</Button>
+            <Button onClick={() => { SetData(<ProductList Category={'Commercial'} />) }}>Commercial RO</Button>
           </li>
           <li>
-            <Button onClick={() => { SetData(<ProductList Category={'Distillation'} />) }}>Ro Filter</Button>
+            <Button onClick={() => { SetData(<ProductList Category={'Industrial'} />) }}>Industrial RO</Button>
           </li>
           <li>
-            <Button onClick={() => { SetData(<ProductList Category={'Ceramic'} />) }}>Industrial</Button>
+            <Button onClick={() => { SetData(<ProductList Category={'Ionizer'} />) }}>Ionizer Machine</Button>
           </li>
           <li>
-            <Button onClick={() => { SetData(<ProductList Category={'Osmosis'} />) }}>Spare Parts</Button>
+            <Button onClick={() => { SetData(<ProductList Category={'Spare'} />) }}>Spare Parts</Button>
           </li>
         </ul>
       </div>
